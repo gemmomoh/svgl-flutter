85f576669d0071da9a6eac283f6d451fd0f26882
gemmomoh <104901333+gemmomoh@users.noreply.github.com>
2026-04-22 00:49:01 +0000

    chore: update svgl components

--- lib/svgl_flutter.dart
+++ lib/svgl_flutter.dart
@@ -1,6 +1,13 @@
 // GENERATED CODE - DO NOT MODIFY BY HAND
 // Run `dart run scripts/generate_exports.dart` to regenerate.
 
+/// Flutter widget library for brand SVG logos powered by svgl.app.
+library svgl_flutter;
+
 export 'src/svgl.dart';
+
+/// Collection of brand SVG logo constants.
 export 'src/svgl_logos.dart';
+
+/// Collection of brand website URLs.
 export 'src/urls.dart';
